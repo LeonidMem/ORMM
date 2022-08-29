fe49eaa8bd6dbe7c211f106dac79918730d173f6
LeonidMem <61764553+LeonidMem@users.noreply.github.com>
2022-08-29 12:50:56 +0300

    Added limit in DelteQuery

--- src/ru/leonidm/ormm/orm/queries/DeleteQuery.java
+++ src/ru/leonidm/ormm/orm/queries/DeleteQuery.java
@@ -10,6 +10,7 @@ import java.util.function.Supplier;
 public final class DeleteQuery<T> extends AbstractQuery<T, Void> {
 
     private Where where;
+    private int limit = 0;
 
     public DeleteQuery(@NotNull ORMTable<T> table) {
         super(table);
@@ -35,6 +36,10 @@ public final class DeleteQuery<T> extends AbstractQuery<T, Void> {
             if(!this.table.getMeta().allowUnsafeOperations()) {
                 throw new UnsafeQueryException("\"WHERE\" is not specified, so the query is unsafe!");
             }
+        }
+
+        if(this.limit != 0) {
+            queryBuilder.append(" LIMIT ").append(this.limit);
         }
 
         return queryBuilder.toString();
